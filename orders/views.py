8eca814edf182b5dbb76c3b1c1d5b043b9601bbe
service <servicemanager3386@yandex.com>
2017-05-16 16:54:03 -0400

    # issued  - checkout page issue

--- orders/views.py
+++ orders/views.py
@@ -17,6 +17,6 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             return render(request, 'orders/order/created.html', {'order': order})
-        else:
-            form = OrderCreateForm()
-        return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
