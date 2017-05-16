1dfc57d90678be348b5f1a2e6e37aedfb87f2c87
service <servicemanager3386@yandex.com>
2017-05-16 15:10:05 -0400

    # created  - cart functionality

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
+from cart.forms import CartAddProductsForm
 
 def product_list(request, category_slug=None):
     category = None
@@ -20,6 +21,8 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductsForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
