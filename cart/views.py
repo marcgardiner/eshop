a56168fd71a910b6199d4a130746bde6f2126885
service <servicemanager3386@yandex.com>
2017-05-16 15:43:16 -0400

    # created  - cart functionality

--- cart/views.py
+++ cart/views.py
@@ -23,4 +23,6 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductsForm(initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
