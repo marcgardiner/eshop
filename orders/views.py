8ee9fe71dc6c9ad202e10f2164913329cc3f599b
service <servicemanager3386@yandex.com>
2017-05-17 09:15:27 -0400

    # created  - url redirected to payment page after completing order

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 from .models import OrderItem
 from .forms import OrderCreateForm
@@ -18,7 +19,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
