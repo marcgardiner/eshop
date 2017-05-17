d0bd775a09ef0a72aa71f99cc812daeebbf0e97c
service <servicemanager3386@yandex.com>
2017-05-17 11:33:43 -0400

    # created  - order detail customization from admin site.

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 import csv
 import datetime
@@ -29,12 +30,12 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
 admin.site.register(Order, OrderAdmin)
 
-
-
-
+def order_detail(obj):
+    return '<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id]))
+order_detail.allow_tags = True
