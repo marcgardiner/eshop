c34d74531670f3c12f693c93860fcb53e089f609
service <servicemanager3386@yandex.com>
2017-05-17 17:52:55 -0400

    # added: applying coupon to order

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from decimal import Decimal
 
 from shop.models import Product
+from coupons.models import Coupon
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -27,6 +30,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, related_name='order_items')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     def __str__(self):
         return '{}'.format(self.id)
