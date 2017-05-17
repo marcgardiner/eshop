fc7d271727ce0ef46227c926dc98f4d6cd694c31
service <servicemanager3386@yandex.com>
2017-05-17 09:14:20 -0400

    # created  - url configuration for payment application

--- eshop/settings.py
+++ eshop/settings.py
@@ -34,13 +34,14 @@ INSTALLED_APPS = (
     'shop',
     'cart',
     'orders',
+    'paypal.standard.ipn',
+    'payment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paypal.standard.ipn',
 )
 
 MIDDLEWARE_CLASSES = (
